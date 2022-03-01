e0a245c491f04a234467ac28c8b4afd4ba9a6d72
daniriian <daniriian@gmail.com>
2022-03-02 00:33:45 +0200

    trying to use authorization with redis...not working

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,51 +1,20 @@
-const jwt = require("jsonwebtoken");
-const redis = require("redis");
+const jwt = require('jsonwebtoken');
+const redis = require('redis');
 
-//setup redis
-const redisClient = redis.createClient({
-  url: process.env.REDIS_URI,
-});
-
-redisClient.connect();
-
-const handleSignin = (db, bcrypt, req, res) => {
-  const { email, password } = req.body;
-  if (!email || !password) {
-    return Promise.reject("incorrect form submission");
-  }
-  return db
-    .select("email", "hash")
-    .from("login")
-    .where("email", "=", email)
-    .then((data) => {
-      const isValid = bcrypt.compareSync(password, data[0].hash);
-      if (isValid) {
-        return db
-          .select("*")
-          .from("users")
-          .where("email", "=", email)
-          .then((user) => user[0])
-          .catch((err) => Promise.reject("unable to get user"));
-      } else {
-        return Promise.reject("wrong credentials");
-      }
-    })
-    .catch((err) => res.status(400).json("wrong credentials"));
-};
-
-const getAuthTokenId = (req, res) => {
-  const { authorization } = req.headers;
-  return redisClient.get(authorization, (err, reply) => {
-    if (err || !reply) {
-      return res.status(400).json("Unauthorized");
-    }
-    return res.json({ id: reply });
+(async () => {
+  //setup redis
+  const redisClient = redis.createClient({
+    url: process.env.REDIS_URI,
   });
-};
+
+  redisClient.on('error', (err) => console.log('Redis Client Error', err));
+
+  await redisClient.connect();
+})();
 
 const signToken = (email) => {
   const jwtPayload = { email };
-  return jwt.sign(jwtPayload, "My_JWT_SuperSecret", { expiresIn: "4 days" });
+  return jwt.sign(jwtPayload, 'My_JWT_SuperSecret', { expiresIn: '4 days' });
 };
 
 const setToken = async (key, value) => {
@@ -57,8 +26,43 @@ const createSessions = (user) => {
   const { id, email } = user;
   const token = signToken(email);
   return setToken(token, id)
-    .then(() => ({ success: "true", userId: id, token }))
+    .then(() => ({ success: 'true', userId: id, token }))
     .catch(console.log);
+};
+
+const handleSignin = (db, bcrypt, req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return Promise.reject('incorrect form submission');
+  }
+  return db
+    .select('email', 'hash')
+    .from('login')
+    .where('email', '=', email)
+    .then((data) => {
+      const isValid = bcrypt.compareSync(password, data[0].hash);
+      if (isValid) {
+        return db
+          .select('*')
+          .from('users')
+          .where('email', '=', email)
+          .then((user) => user[0])
+          .catch((err) => res.status(400).json('unable to get user'));
+      } else {
+        return Promise.reject('wrong credentials');
+      }
+    })
+    .catch((err) => err);
+};
+
+const getAuthTokenId = (req, res) => {
+  const { authorization } = req.headers;
+  return redisClient.get(authorization, (err, reply) => {
+    if (err || !reply) {
+      return res.status(401).send('Unauthorized');
+    }
+    return res.json({ id: reply });
+  });
 };
 
 const signinAuthentication = (db, bcrypt) => (req, res) => {
@@ -74,9 +78,34 @@ const signinAuthentication = (db, bcrypt) => (req, res) => {
             : Promise.reject(data);
         })
         .then((session) => res.json(session))
-        .catch((err) => res.status(400).json("Eroare"));
+        .catch((err) => res.status(400).json('Error'));
+};
+
+const requireAuth = (req, res, next) => {
+  const { authorization } = req.headers;
+  if (!authorization) {
+    return res.status(401).send('Unauthorized');
+  }
+
+  //First check in Redis
+  console.log('first check in redis');
+  const value = redisClient.get(authorization, (err, data) => {
+    console.log('getiing redisClient');
+    if (err) {
+      console.log(err);
+    }
+    if (data) {
+      console.log(data);
+      const reponse = JSON.parse(data);
+      return res.status(200).json(reponse);
+    }
+    console.log('Should pass');
+    return next();
+  });
+  console.log(value);
 };
 
 module.exports = {
-  signinAuthentication: signinAuthentication,
+  signinAuthentication,
+  requireAuth,
 };
